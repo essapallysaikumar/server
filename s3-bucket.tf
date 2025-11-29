e8967821f5bf0ecbcc2964e59beeaedbb227dfd1
ESSAPALLYSAIKUAMR <essapallysaikumars@gmail.com>
2025-11-29 11:24:17 +0530

    Add AWS instance resource to s3-bucket.tf

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,9 +1,13 @@
 provider "aws" {
-resource = "aws_s3_bucket" "one" 
-  bucket = "qwertyu876543"
+  region = "us-west-2"
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-025ca978d4c1d9825" # Replace with a valid AMI ID
+  instance_type = "t2.micro"
+  key_name      = "my-ssh-key" # Replace with your key pair name
 
   tags = {
-    Name        = "My bucket"
-    Environment = "Dev"
+    Name = "ExampleInstance"
   }
 }
